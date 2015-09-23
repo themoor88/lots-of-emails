667dfc30b69d36a38f05a408c81855561069bd25
WajidKagzi <wjkagzi@gmail.com>
2015-09-23 19:24:47 -0400

    remove redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super do
       UserNotifier.welcome_email(@user).deliver
-      redirect_to(root_path, :notice => 'User created')
     end
   end
 end
