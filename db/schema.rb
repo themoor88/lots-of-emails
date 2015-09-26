69afd9281b4266c8b2cd1f1954f47c064467fffc
WajidKagzi <wjkagzi@gmail.com>
2015-09-26 18:14:54 -0400

    Mysql works and registrations work.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20150922010551) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
