df579b5aabe905edb442aa64696bc34bd1056960
WajidKagzi <wjkagzi@gmail.com>
2015-10-29 23:04:26 -0400

    Adding email credentials for sendgrid.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'wkagzi',
+  :password => 'sarahalidoost',
+  :domain => 'devbbq.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
