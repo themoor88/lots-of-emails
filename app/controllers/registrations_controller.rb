70aa5e54780b10239015510bae42a461c1bbc890
WajidKagzi <wjkagzi@gmail.com>
2015-10-31 12:24:23 -0400

    Email templates working and responsive.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super {
       if @user.persisted?
-        UserNotifier.welcome_email(@user).deliver
+        UserNotifier.welcome_email(@user).deliver_now
       end
     }
   end
--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -5,9 +5,9 @@ class UserNotifier < ApplicationMailer
     template = SendGrid::Template.new('c8ac2b43-365c-400e-9009-607700fb1f90')
     recipients = [SendGrid::Recipient.new(@user.email)]
     mailer_defaults = {
-      from: 'wjkagzi@gmail.com',
-      subject: 'what',
-      text: 'what'
+      from: 'noreply@gmail.com',
+      subject: ' ',
+      html: '<div></div>'
     }
     mailer = SendGrid::TemplateMailer.new(client, template, recipients)
     mailer.mail(mailer_defaults)
