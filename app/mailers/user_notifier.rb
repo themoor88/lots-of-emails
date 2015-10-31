6aaf5f95acf5419e7204393fa84a113a4de74718
WajidKagzi <wjkagzi@gmail.com>
2015-10-31 15:05:29 -0400

    Finishing up email templates, still have to render variables in templates.

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -5,7 +5,7 @@ class UserNotifier < ApplicationMailer
     template = SendGrid::Template.new('c8ac2b43-365c-400e-9009-607700fb1f90')
     recipients = [SendGrid::Recipient.new(@user.email)]
     mailer_defaults = {
-      from: 'noreply@gmail.com',
+      from: 'wjkagzi@gmail.com',
       subject: ' ',
       html: '<div></div>'
     }
