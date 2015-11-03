29e56eafe22d013287e5a20175f98356ef5f0483
WajidKagzi <wjkagzi@gmail.com>
2015-11-03 11:03:17 -0500

    DRYing up the code and adding comments.

--- app/controllers/concerns/emailable.rb
+++ app/controllers/concerns/emailable.rb
@@ -1,16 +1,20 @@
 module Emailable
   extend ActiveSupport::Concern
 
-  def send_email(email_parameters, substitutions, template_id)
-    header = Smtpapi::Header.new
+  def send_email(email_info)
+    # Initiating the SendGrid API
     client = SendGrid::Client.new(api_user: ENV['MAILER_USERNAME'], api_key: ENV['MAILER_PASSWORD'])
 
-    header.add_filter('templates', 'template_id', template_id)
+    # Using the SMTP API header to pass client information
+    header = Smtpapi::Header.new
 
-    substitutions.each do |sub|
-      header.add_substitution(sub[0], [sub[1]])
-    end
+    # Specifying the template
+    header.add_filter('templates', 'template_id', email_info[:template_id])
 
+    # Adding substitution tags for the template
+    email_info[:substitutions].each {|sub| header.add_substitution(sub[0], [sub[1]])}
+
+    # Parameters required for the API to work properly
     required_parameters = {
       text: ' ',
       html: ' ',
@@ -19,8 +23,30 @@ module Emailable
       smtpapi: header
     }
 
-    email_parameters.merge!(required_parameters)
-    message = SendGrid::Mail.new(email_parameters)
+    # Adding the required parameters to the original parameters
+    email_info[:email_parameters].merge!(required_parameters)
+
+    # Initiating the message and sending the it along the API
+    message = SendGrid::Mail.new(email_info[:email_parameters])
     client.send(message)
+  end
+
+  def send_confirmation_email(resource)
+    email_info = {
+      email_parameters: {
+        to: resource.email,
+        to_name: resource.first_name,
+        subject: 'Thanks for joining Therapia!'
+      },
+
+      substitutions: {
+        '-firstName-': resource.first_name,
+        '-lastName-': resource.last_name
+      },
+
+      template_id: 'c8ac2b43-365c-400e-9009-607700fb1f90'
+    }
+
+    send_email(email_info)
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,19 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super {
       if @user.persisted?
-        email_parameters = {
-          to: @user.email,
-          to_name: @user.first_name,
-          subject: 'Thanks for joining Therapia!'
-        }
-
-        substitutions = {
-          '-firstName-' => 'Cool',
-          '-lastName-' => 'Beans'
-        }
-
-        template_id = 'c8ac2b43-365c-400e-9009-607700fb1f90'
-        send_email(email_parameters, substitutions, template_id)
+        send_confirmation_email(@patient)
       end
     }
   end
